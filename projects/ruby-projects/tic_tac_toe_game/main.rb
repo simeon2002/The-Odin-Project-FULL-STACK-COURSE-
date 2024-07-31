ed4b0cfb43f2e0533daac34466b673f1962cf731
Simeon <61100437+simeon2002@users.noreply.github.com>
2024-07-31 18:05:36 +0200

    Add exit message when player doens't want to play anymore

--- projects/ruby-projects/tic_tac_toe_game/main.rb
+++ projects/ruby-projects/tic_tac_toe_game/main.rb
@@ -6,12 +6,15 @@ require 'debug'
 # setup game.
 game = Game.new
 game.setup_game
+game.play_game
 loop do
-  game.play_game
   puts
   puts 'Want to play another game? (answer with a yes or no)'
-  break unless gets.chomp == 'yes'
-
+  unless gets.chomp == 'yes'
+    puts 'well, thanks for playing the game!'
+    break
+  end
+  puts
   game.reset
   game.play_game
 end
